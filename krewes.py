209427fdc4a6b7abddefb0397669978f60fe745a
klin50 <kevinl713@nycstudents.net>
2024-09-15 19:24:32 -0400

    random

--- krewes.py
+++ krewes.py
@@ -1,3 +1,8 @@
+import random
 krewes = open("krewes","r")
-def random():
-    print(krewes.read())
+data = krewes.read()
+krewes_list = data.replace('\n', ' ').split(",")
+def randomNum():
+    x = random.randint(0,len(krewes_list)-1)
+    print(krewes_list[x].replace(" ",""))
+randomNum()
