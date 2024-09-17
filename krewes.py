167ab7a31f166cc3d62d365bd729877213c24938
klin50 <kevinl713@nycstudents.net>
2024-09-16 22:20:15 -0400

    K04

--- krewes.py
+++ krewes.py
@@ -1,13 +1,27 @@
 #Kevin Lin
+#Death Row Coders
 #SoftDev
 #Dictionary and List Review
 #2024/9/15
 #1 hour
 import random
-krewes = open("krewes","r")
-data = krewes.read()
-krewes_list = data.replace('\n', ' ').split(",")
+krewes = {
+           4: [
+               'DUA','TAWAB','EVA','JACK','VICTOR','EVAN','JASON','COLYI','IVAN','TANZEEM',
+                'TAHMIM','STANLEY','LEON','NAOMI','NIA','ANASTASIA','JADY','BRIAN','JACOB',
+               'ALEX','CHONGTIAN','DANNY','MARCO','ABIDUR','ANKITA','ANDY','ETHAN','AMANDA',
+               'AIDAN','LINDA','QIANJUN','JIAYING','KISHI'
+               ],
+           5: [ 
+                'ADITYA','MARGIE','RACHEL','ALEXANDER','ZIYAD','DANNY','ENDRIT','CADEN',
+                'VEDANT','SUHANA','KYLE','KEVIN','RAYMOND','CHRISTOPHER','JONATHAN','SASHA',
+                'NAFIYU','TIM','WILL','DANIEL','BENJAMIN','CLAIRE','CHLOE','STELLA','TRACY',
+                'JESSICA','JACKIE','WEN YUAN','YINWEI','TIFFANY','JAYDEN DANIEL','PRINCEDEN' 
+              ]
+         }
+
 def randomNum():
-    x = random.randint(0,len(krewes_list)-1)
-    print(krewes_list[x].replace(" ",""))
+    x = random.randint(4,5)
+    y = krewes.get(x)
+    print(y[random.randint(0,len(y)-1)])
 randomNum()
