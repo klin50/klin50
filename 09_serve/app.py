f56aece84652282f31fe956838a4e5227a5eecab
Kevin Lin <klin50@stuy.edu>
2024-09-25 11:21:15 -0400

    09_serve

--- 09_serve/app.py
+++ 09_serve/app.py
@@ -1,32 +1,53 @@
-#Kevin Lin, Suhana Kumar, Caden Khuu
-#Hello
-#SoftDev
-#Using flask to print occupation results on webpage
-#2024-9-24
-#0.5
-import csv
+# Kevin Lin
+# Death Row Coders
+# SoftDev
+# K09 <CSV file parsing, flask, displaying to HTML>
+# 2024-9-24
+# Time Spent : 0.5 Hours
+
 import random
+import csv
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route("/")
-def occupation():
-    with open('occupations.csv', "r") as file: #Don't have to close
-        csvFile = csv.reader(file)
-        info = {}
-        count = 0
-        rando_number = random.randint(0,998)
-        total = 0
-        for lines in csvFile:
-            if not(count == 0): #Removes the first value in the list
-                if (total > rando_number):
-                    print(lines[0])
-                    return(lines[0])
-                else:
-                    total += (float(lines[1])*10)
-                info.update({lines[0]:float(lines[1])})  #Add to dictionary
-            count = count + 1
-        return ("Hello\n" + info.popitem()) 
+def readfile(f):
+    d = {}
+    with open (f, 'r') as listfile:
+        reader = csv.reader(listfile)
+        next(reader)
+        for row in reader:
+            job = row[0]
+            if job == "Total":
+                continue
+            percent = float(row[1])
+            d[job] = percent
+    return d
+        
+        
+def sel(d):
+    return random.choices(list(d.keys()), weights=d.values(), k=1)[0]
 
-if __name__ == '__main__':
+@app.route("/")
+
+def page():
+    occ = sel(readfile("occupations.csv"))
+    code = """
+    <!DOCTYPE html>
+    <html>
+      <body>
+            <p>Death Row Coders with Raymond, Christopher, and Kevin.</p>
+            <h1>This time: """ + occ + """
+            <h2>Occupations</h2>
+    """
+    for a, b in readfile("occupations.csv").items():
+        print(a)
+        print(b)
+        code += "<li>" + a + ": " + str(b) + "</li>"
+
+    code += "</body></html>"
+    return code
+
+if __name__ == "__main__":
+    app.debug = True
     app.run()
