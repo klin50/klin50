96e2b8673c3966a8d4700259fce14d3af4da0a8a
Kevin Lin <klin50@stuy.edu>
2024-10-01 14:50:29 -0400

    data

--- 13_tempwork/app.py
+++ 13_tempwork/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import random, csv
 app = Flask(__name__)
 
 def readfile(f):
@@ -24,7 +25,7 @@ def hello_world():
 
 @app.route("/template")
 def test_tmplt():
-    return render_template( 'tablified.html', collection1=readfile("occupations.csv").keys(), collection2=readfile("occupations.csv").values())
+    return render_template('tablified.html', collection1=readfile("data/occupations.csv").keys(), collection2=readfile("data/occupations.csv").values())
 
 if __name__ == "__main__":
     app.debug = True
