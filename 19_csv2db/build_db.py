050f54ac601f694577e27c1926eee1d29a79fafb
Kevin Lin <klin50@stuy.edu>
2024-10-18 11:45:45 -0400

    19

---
+++ 19_csv2db/build_db.py
@@ -0,0 +1,34 @@
+#Clyde "Thluffy" Sinclair
+#SoftDev
+#skeleton/stub :: SQLITE3 BASICS
+#Oct 2024
+
+import sqlite3   #enable control of an sqlite database
+import csv       #facilitate CSV I/O
+
+
+DB_FILE="discobandit.db"
+
+db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
+
+#==========================================================
+
+
+"""
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+< < < INSERT YOUR TEAM'S DB-POPULATING CODE HERE > > >
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+with open('courses.csv', 'r') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        (row['code'],row['mark'])
+
+command = ""          # test SQL stmt in sqlite3 shell, save as string
+c.execute(command)    # run SQL statement
+
+#==========================================================
+
+db.commit() #save changes
+db.close()  #close database
