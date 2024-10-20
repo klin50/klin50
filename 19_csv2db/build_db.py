b733750e9f3a9b00f352e471a255fc33999b3066
klin50 <kevinl713@nycstudents.net>
2024-10-19 22:48:56 -0400

    19

--- 19_csv2db/build_db.py
+++ 19_csv2db/build_db.py
@@ -20,14 +20,31 @@ c = db.cursor()               #facilitate db ops -- you will use cursor to trigg
 < < < INSERT YOUR TEAM'S DB-POPULATING CODE HERE > > >
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
+
+command = "CREATE TABLE courses(code TEXT, mark INTEGER, id INTEGER);"          # test SQL stmt in sqlite3 shell, save as string
+c.execute(command)    # run SQL statement
+command = "CREATE TABLE students(name TEXT, age INTEGER, id INTEGER);"
+c.execute(command)
+
 with open('courses.csv', 'r') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        (row['code'],row['mark'])
+        c.execute("INSERT INTO courses(code,mark,id) VALUES (?,?,?)", (row['code'],row['mark'],row['id']))
 
-command = ""          # test SQL stmt in sqlite3 shell, save as string
-c.execute(command)    # run SQL statement
+c.execute('SELECT * FROM courses') #Reads the table
+table = c.fetchall() #Gets the data after reading it
+for tab in table:
+    print(tab)
 
+with open('students.csv', 'r') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        c.execute("INSERT INTO students(name,age,id) VALUES (?,?,?)", (row['name'],row['age'],row['id']))
+
+c.execute('SELECT * FROM students') #Reads the table
+table = c.fetchall() #Gets the data after reading it
+for tab in table:
+    print(tab)
 #==========================================================
 
 db.commit() #save changes
