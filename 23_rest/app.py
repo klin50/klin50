dd2f4a7c576dda3e1236d22b72a71a7b32ef684b
Kevin Lin <klin50@stuy.edu>
2024-11-20 11:47:19 -0500

    23

---
+++ 23_rest/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+import urllib.request
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def nasa():
+    with urllib.request.urlopen('https://api.nasa.gov/planetary/apod?api_key=qcOS0PSRF5SfhDAepZCyjfvLVYWAydsFsAEc7wxT') as response:
+        html = response.read()
+        data = json.loads(html)
+        print(data)
+    return render_template("main.html")
+    
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
