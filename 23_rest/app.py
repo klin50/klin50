d744f736da18cc4aab7db869b492f1a7159a6d07
Kevin Lin <klin50@stuy.edu>
2024-11-20 11:47:45 -0500

    updated

--- 23_rest/app.py
+++ 23_rest/app.py
@@ -9,7 +9,7 @@ def nasa():
     with urllib.request.urlopen('https://api.nasa.gov/planetary/apod?api_key=qcOS0PSRF5SfhDAepZCyjfvLVYWAydsFsAEc7wxT') as response:
         html = response.read()
         data = json.loads(html)
-        print(data)
+        
     return render_template("main.html")
     
 if __name__ == "__main__":
